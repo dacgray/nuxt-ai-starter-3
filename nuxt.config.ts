fc19e79d1101621a25e99852430f739ae301f3d8
David Gray <dacgray@gmail.com>
2026-03-11 17:18:27 +0700

    Create nuxt app
    npm create nuxt@latest

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxt/test-utils',
+    '@nuxtjs/i18n',
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-04-03',
+})
